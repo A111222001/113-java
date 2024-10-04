895ee587b62066f094fc44c2928dab632d8ebd2c
A111222001 <A111222001@mail.shu.edu.tw>
2024-10-04 11:41:56 +0800

    113-10-04

--- 113-10-04/SecureRandomDemo.java
+++ 113-10-04/SecureRandomDemo.java
@@ -1,9 +1,10 @@
+import java.security.SecureRandom;
 
-
-public class RandomStop {
+public class SecureRandomDemo {
     public static void main(String[] args) {
+        SecureRandom randomNumber = new SecureRandom();
         while(true) {
-            var number = (int) (Math.random() * 10);
+            var number = randomNumber.nextInt(10);
             System.out.println(number);
             
             if(number == 5) {
