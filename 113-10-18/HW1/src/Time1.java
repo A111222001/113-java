aca32d3d7fd8311a3cdcf9755d46ed7ff8144745
A111222001 <A111222001@mail.shu.edu.tw>
2024-10-18 12:00:05 +0800

    113-10-18

---
+++ 113-10-18/HW1/src/Time1.java
@@ -0,0 +1,15 @@
+public class Time1 {
+    //時(hour)
+    //分(minute)
+    //秒(second)
+
+    //方法:建構子(是定時分秒的初始值，需檢查資料是否格式正確)
+
+    //方法:是需要加入get/set
+
+    //方法:tick (時間增加1秒)
+
+    //方法:printUniversal (以24小時制顯示時間)
+
+    //方法:printStandard (以12小時制顯示時間)
+}
